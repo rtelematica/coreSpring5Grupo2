61543da96cb45d312c1730e02e4ce5eb97e85fd9
Ivan Garcia <isc.ivgarcia@gmail.com>
2020-02-08 12:56:23 -0600

    Clase 6 - Method replacement

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica21/methodreplacement/replacer/RunReplacer.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica21/methodreplacement/replacer/RunReplacer.java
@@ -1,9 +1,25 @@
 package org.certificatic.spring.core.practica21.methodreplacement.replacer;
 
+import java.lang.reflect.Method;
+
+import org.certificatic.spring.core.practica21.methodreplacement.bean.SportCar;
+import org.springframework.beans.factory.support.MethodReplacer;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 // Implementar MethodReplacer
-public class RunReplacer {
+public class RunReplacer implements MethodReplacer {
+
+	@Override
+	public Object reimplement(Object obj, Method method, Object[] args) throws Throwable {
+		int mills = 200;
+
+		SportCar car = (SportCar) obj;
+
+		log.info("Sport car model: {} is running at: {}...", car.getModel(), mills);
+
+		return mills;
+	}
 
 }
