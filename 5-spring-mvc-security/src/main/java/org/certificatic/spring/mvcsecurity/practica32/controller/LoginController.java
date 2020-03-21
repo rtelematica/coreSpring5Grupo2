f68eeb3373e45e8c5a71a39a0e8496140cb5db61
Ivan Garcia <isc.ivgarcia@gmail.com>
2020-03-21 16:39:17 -0600

    Clase 12 - Spring Security Form Login

--- 5-spring-mvc-security/src/main/java/org/certificatic/spring/mvcsecurity/practica32/controller/LoginController.java
+++ 5-spring-mvc-security/src/main/java/org/certificatic/spring/mvcsecurity/practica32/controller/LoginController.java
@@ -1,7 +1,11 @@
 package org.certificatic.spring.mvcsecurity.practica32.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,12 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 public class LoginController {
 
 	// Anotar request mapping a "", "/" y "/login", por metodo GET
-	public String showLoginPage(Model model) {
+	@GetMapping({"", "/", "/login"})
+	public String showLoginPage(Model model, 
+			@RequestParam(required = false, value = "error") boolean error) {
 
 		log.info("show login page ------------------");
 
 		// Verificar si existe error en la autenticacion
 		// Si existe error agregar mensaje de error al Modelo
+		if(error) {
+			log.info("login has error");
+			model.addAttribute("errorMessage", "Wrong user or password");
+		}
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		log.info("authentication: {}", auth);
 
 		model.addAttribute("currentSecc", "login");
 
@@ -23,6 +36,7 @@ public class LoginController {
 	}
 
 	// Anotar request mapping a "/access-denied", por metodo GET
+	@GetMapping("/access-denied")
 	public String showAccessDeniedPage() {
 
 		log.info("show access-denied page ------------------");
