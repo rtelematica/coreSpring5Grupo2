887a20e7c3d756d909305336169435aada671b35
Ivan Garcia <isc.ivgarcia@gmail.com>
2020-03-21 13:23:23 -0600

    Clase 12 - Spring MVC REST

--- 5-spring-mvc/src/main/java/org/certificatic/spring/mvc/practica31/controller/PersonsController.java
+++ 5-spring-mvc/src/main/java/org/certificatic/spring/mvc/practica31/controller/PersonsController.java
@@ -8,14 +8,23 @@ import javax.annotation.PostConstruct;
 
 import org.certificatic.spring.validation.practica30.parte1.domain.Person;
 import org.certificatic.spring.validation.practica30.parte1.domain.Persons;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// Anotar RestController
+// Anotar Controller
+@Controller
 // Anotar request mapping "/persons"
+@RequestMapping("/persons")
 public class PersonsController {
 
 	private List<Person> persons = Collections.synchronizedList(new ArrayList<Person>());
@@ -32,18 +41,26 @@ public class PersonsController {
 	}
 
 	// Anotar request mapping "/", "", con metodo GET y produciendo json y xml
-	public Persons getAllPersons() {
+	@RequestMapping(value = {"/", ""}, method = RequestMethod.GET,
+			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public @ResponseBody Persons getAllPersons() {
 		return new Persons(persons);
 	}
 
 	// Anotar request mapping "/{id}", con metodo GET y produciendo json y xml
 	// Anotar response status ok
-	public Person getPerson(@PathVariable Integer id) {
+	@RequestMapping(value = {"/{id}"}, method = RequestMethod.GET,
+			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@ResponseStatus(HttpStatus.OK)
+	public @ResponseBody Person getPerson(@PathVariable Integer id) {
 		return persons.get(id - 1);
 	}
 
 	// Anotar request mapping "/", "", con metodo POST y produciendo json y xml
 	// Anotar response status NO CONTENT
+	@RequestMapping(value = {"/", ""}, method = RequestMethod.POST,
+			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void createPerson(@RequestBody Person person) {
 		persons.add(person);
 	}
--- 5-spring-mvc/src/main/java/org/certificatic/spring/mvc/practica31/controller/advice/ErrorHandlingControllerAdvice.java
+++ 5-spring-mvc/src/main/java/org/certificatic/spring/mvc/practica31/controller/advice/ErrorHandlingControllerAdvice.java
@@ -1,15 +1,28 @@
 package org.certificatic.spring.mvc.practica31.controller.advice;
 
+import org.certificatic.spring.mvc.practica31.controller.PersonsController;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 //Anotar Controller Advice, asignar tipos a PersonsController.class
+@ControllerAdvice(assignableTypes = { PersonsController.class })
 public class ErrorHandlingControllerAdvice {
 
 	// Anotar ExceptionHandler
-	// Anotar Response status (internal server error)
+	@ExceptionHandler(IndexOutOfBoundsException.class)
+	// Anotar Response status 404 NOT FOUND
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	// Anotar ResponseBody
-	public RestResponseError onException(RuntimeException re) {
-		return new RestResponseError(HttpStatus.INTERNAL_SERVER_ERROR, re.getMessage(), re.getClass().getSimpleName());
+	@ResponseBody
+	public RestResponseError onException(IndexOutOfBoundsException re) {
+		return new RestResponseError(
+				HttpStatus.NOT_FOUND, 
+				re.getMessage(), 
+				re.getClass().getSimpleName());
 	}
 
 }
